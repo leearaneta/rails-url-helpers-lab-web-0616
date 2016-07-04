2c0ae7fbdd7604cc79735aaee8806ca3f8f5b092
leearaneta <lee.araneta@gmail.com>
2016-07-04 12:09:02 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
-  gem 'byebug'
+  gem 'pry-byebug'
   gem 'web-console', '~> 2.0'
   gem 'spring'
   gem 'rspec-rails', '~> 3.0.0'
@@ -32,4 +32,3 @@ group :development, :test do
   gem 'capybara'
   gem 'database_cleaner'
 end
-
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,20 @@
+require 'pry'
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    @student.switch
+    @student.save
+    redirect_to "/students/#{@student.id}"
   end
 
   private
